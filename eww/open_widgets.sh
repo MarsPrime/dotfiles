17fd80d7f83521a789ca6cbb2f95ac37da8ce143
demid <demid090605@outlook.com>
2025-07-26 18:26:49 +0700

    Fix: - volume widgets 	изменено:      eww/eww.yuck 	изменено:      eww/open_widgets.sh

--- eww/open_widgets.sh
+++ eww/open_widgets.sh
@@ -5,7 +5,7 @@ eww open battery_panel_2
 eww open battery_time
 eww open battery_status_1
 eww open battery_status_2
-eww open battery_status_2
+eww open battery_status_3
 eww open memory_status
 eww open volume_panel1 
 eww open volume_panel2 
@@ -13,4 +13,7 @@ eww open volume_panel3
 eww open volume_panel4 
 eww open volume_panel5 
 eww open volume_panel6 
+eww open volume_bar1
+eww open volume_bar2
+eww open volume_bar3
 
