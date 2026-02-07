dea9f58b12a59e5f39560be79c082591baf59763
MarsPrime <132182742+MarsPrime@users.noreply.github.com>
2026-02-07 11:05:05 +0700

    New bar config
    - Add new polybar config
    - Add zsh config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+pkill polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log /tmp/polybar4.log
-M
-MONITOR=eDp-1 polybar workspaces 2>&1 | tee -a /tmp/polybar1.log & disown
-MONITOR=eDp-1 polybar data-panel 2>&1 | tee -a /tmp/polybar2.log & disown
-MONITOR=eDp-1 polybar date-panel 2>&1 | tee -a /tmp/polybar3.log & disown
-MONITOR=eDp-1 polybar memory 2>&1 | tee -a /tmp/polybar4.log & disown
-MONITOR=eDp-1 polybar cpu 2>&1 | tee -a /tmp/polybar5.log & disown
+MONITOR=eDp-1 polybar -c ~/.config/polybar/bars/date-bar.ini date &
+MONITOR=eDp-1 polybar -c ~/.config/polybar/bars/workspaces.ini workspaces &
+MONITOR=eDp-1 polybar -c ~/.config/polybar/bars/system-tray.ini system-tray &
+MONITOR=eDp-1 polybar -c ~/.config/polybar/bars/status-bar.ini status-bar
 
-echo "Bars launched..."
